582c67446d8efb443a2e179bd99c2afa1cac2a58
Arvis <viestards.lists@gmail.com>
2023-02-28 15:49:58 +0200

    write my own write to file

--- src/main/java/me/arvis/Person/aspect/LoggingAspect.java
+++ src/main/java/me/arvis/Person/aspect/LoggingAspect.java
@@ -1,6 +1,5 @@
 package me.arvis.Person.aspect;
 
-import me.arvis.Person.config.LoadDatabase;
 import me.arvis.Person.dao.LogDao;
 import me.arvis.Person.dao.PersonDao;
 import me.arvis.Person.repository.LogRepository;
@@ -12,6 +11,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 @Aspect
@@ -21,6 +23,8 @@ public class LoggingAspect {
     @Autowired
     private LogRepository repository;
 
+    private String fileName = "aspect_log.log";
+
     private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
 
     @AfterReturning(
@@ -29,7 +33,8 @@ public class LoggingAspect {
     public void afterReturningAllPersonAdvice(
             JoinPoint theJoinPoint, List<PersonDao> result) {
         repository.save(new LogDao("searched all items: " + result));
-        log.info("\n=====>>> searched all items: " + result);
+        writeToFile("searched all items: " + result);
+
     }
 
     @AfterReturning(
@@ -39,7 +44,18 @@ public class LoggingAspect {
             JoinPoint theJoinPoint, List<PersonDao> result) {
 
         repository.save(new LogDao("searched with filters: " + result));
-        log.info("\n=====>>> checked for filters: " + result);
+        writeToFile("checked for filters: " + result);
+    }
+
+    private void writeToFile(String textToWrite){
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
+            writer.append("\n " +textToWrite);
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 
